91d1c8c201ba8d9f9ea98858b2804a1a98db8231
Ekaterina Malygina <katty2007v@yandex.ru>
2021-03-24 23:29:58 +0300

    lesson-12

--- src/main/java/geekbrains/spring/lesson10/exceptions/SessionExpiredException.java
+++
@@ -1,7 +0,0 @@
-package geekbrains.spring.lesson10.exceptions;
-
-public class SessionExpiredException extends RuntimeException{
-    public SessionExpiredException(String message) {
-        super(message);
-    }
-}
