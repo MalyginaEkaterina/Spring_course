16341f3663de0b927852a08475ac94dcb8f9c22f
Ekaterina Malygina <katty2007v@yandex.ru>
2021-06-21 21:02:53 +0300

    arch-lesson-7

--- src/main/java/com/geekbrains/spring_boot/Product.java
+++ src/main/java/com/geekbrains/spring_boot/Product.java
@@ -1,10 +1,20 @@
 package com.geekbrains.spring_boot;
 import lombok.*;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "products")
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Product {
-    private int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Integer id;
+    @Column
     private String title;
+    @Column
     private double cost;
 }
